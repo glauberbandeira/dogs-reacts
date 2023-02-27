d9becc2b2281fad733b5ac053ea3579a53e25b61
Glauber Bandeira <glauberbandeira_@hotmail.com>
2023-02-27 22:46:56 +1100

    Login Create

--- src/Hooks/useForm.js
+++ src/Hooks/useForm.js
@@ -6,7 +6,12 @@ const types = {
         ,
         message: 'Preencha um email válido',
     },
-}
+    password: {
+        regex: /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])[0-9a-zA-Z]{8,}$/
+        ,
+        message: 'A senha precisa ter 1 caracter maíusculo, 1 minúsculo e 1 digito. Com no mínimo 8 characters.',
+    }
+};
 
 const useForm = (type) => {
     const [value, setValue] = React.useState('');
