3990b0e5105611f3ecc6917d993aa7b855d8ac0d
Glauber Bandeira <glauberbandeira_@hotmail.com>
2023-03-03 20:12:18 +1100

    did clone code

--- src/Hooks/useFetch.js
+++ src/Hooks/useFetch.js
@@ -1,35 +1,35 @@
-import React from 'react'
+import React from 'react';
 
 const useFetch = () => {
-    const [data, setData] = React.useState(null);
-    const [error, setError] = React.useState(null);
-    const [loading, setLoading] = React.useState(false);
+  const [data, setData] = React.useState(null);
+  const [error, setError] = React.useState(null);
+  const [loading, setLoading] = React.useState(false);
 
-    const request = React.useCallback(async (url, options) => {
-        let response;
-        let json;
-        try {
-            setError(null)
-            setLoading(true);   
-            response = await fetch(url, options);
-            json = await response.json()
-            if(response.ok === false) throw new Error(json.message);
-        } catch (err) {
-            json = null;
-            setError(err.message);
-        } finally {
-            setData(json)
-            setLoading(false);
-            return {response, json}
-        }
-    }, [])
+  const request = React.useCallback(async (url, options) => {
+    let response;
+    let json;
+    try {
+      setError(null);
+      setLoading(true);
+      response = await fetch(url, options);
+      json = await response.json();
+      if (response.ok === false) throw new Error(json.message);
+    } catch (err) {
+      json = null;
+      setError(err.message);
+    } finally {
+      setData(json);
+      setLoading(false);
+      return { response, json };
+    }
+  }, []);
 
   return {
     data,
-    loading, 
+    loading,
     error,
-    request
-  }
-}
+    request,
+  };
+};
 
-export default useFetch
+export default useFetch;
