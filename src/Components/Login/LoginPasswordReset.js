16fe7e8d61c868c11de27594a7fc2d880eb6c563
Glauber Bandeira <glauberbandeira_@hotmail.com>
2023-03-14 21:05:56 +1100

    Head

--- src/Components/Login/LoginPasswordReset.js
+++ src/Components/Login/LoginPasswordReset.js
@@ -6,6 +6,7 @@ import useFetch from '../../Hooks/useFetch';
 import { PASSWORD_RESET } from '../../api';
 import Error from '../Helper/Error';
 import { useNavigate } from 'react-router-dom';
+import Head from '../Helper/Head';
 
 
 const LoginPasswordReset = () => {
@@ -37,6 +38,7 @@ const LoginPasswordReset = () => {
   }
 
   return <div>
+    <Head title="Reseta a senha"/>
     <h1 className='title'>Resete a senha</h1>
     <form onSubmit={handleSubmit}>
       <Input label='Nova senha' type="password" name="password" {...password} />
--- src/Components/User/UserStats.js
+++ src/Components/User/UserStats.js
@@ -1,7 +1,11 @@
 import React from 'react';
+import Head from '../Helper/Head';
 
 const UserStats = () => {
-  return <div>Estatísticas</div>;
+  return <div>
+    <Head title="Estátisticas" description="Home do site Dogs, com o feed de fotos."/>
+    Estatísticas
+    </div>;
 };
 
 export default UserStats;
